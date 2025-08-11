969710ec6bdece6832315a9904a71dcd85ccd874
Paul S Flanagan <paulsflanagan@gmail.com>
2025-08-11 22:05:32 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,5 +5,10 @@ st.title('Streamlit App Public - Two')
 try:
   url = st.query_params["url"]
   st.write("URL FOUND " + url)
+
+  if url == "1234":
+    st.write("Success")
+  else:
+    st.write("Failure")
 except:
-  st.write("URL NOT FOUND")
+  st.write("Failure no url")
